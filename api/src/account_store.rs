f26b1cda574fd1314b9e96dc182e070b4a8aeb4f
Konrad Schultz <konrad@seam.io>
2025-09-22 11:56:32 -0700

    Move account store into its own file

---
+++ api/src/account_store.rs
@@ -0,0 +1,108 @@
+use sqlx::{Pool, Postgres, types::time::OffsetDateTime};
+use std::error::Error;
+
+#[derive(sqlx::FromRow, Debug)]
+pub struct AccountRow {
+    pub id: i64,
+    pub first_name: String,
+    pub last_name: String,
+    pub email: String,
+    // TODO: do the hashing
+    pub password_hash: String,
+
+    // TODO: Use chrono... or this fine??
+    pub created_at: OffsetDateTime,
+    pub updated_at: OffsetDateTime,
+}
+
+pub struct InsertAccount {
+    pub first_name: String,
+    pub last_name: String,
+    pub email: String,
+    pub password_hash: String,
+}
+
+pub struct UpdateAccount {
+    pub first_name: String,
+    pub last_name: String,
+}
+
+pub trait AccountStore {
+    async fn create(&self, params: InsertAccount) -> Result<AccountRow, Box<dyn Error>>;
+    async fn update(&self, id: i64, params: UpdateAccount) -> Result<AccountRow, Box<dyn Error>>;
+    async fn delete(&self, id: i64) -> Result<AccountRow, Box<dyn Error>>;
+    async fn get(&self, id: i64) -> Result<AccountRow, Box<dyn Error>>;
+    async fn list(&self) -> Result<Vec<AccountRow>, Box<dyn Error>>;
+}
+
+pub struct PsqlAccountStore {
+    db: Pool<Postgres>,
+}
+
+impl PsqlAccountStore {
+    pub fn new(db: Pool<Postgres>) -> Self {
+        Self { db }
+    }
+}
+
+impl AccountStore for PsqlAccountStore {
+    async fn create(&self, params: InsertAccount) -> Result<AccountRow, Box<dyn Error>> {
+        Ok(sqlx::query_as!(
+            AccountRow,
+            "
+            INSERT INTO account
+            (first_name, last_name, email, password_hash, created_at, updated_at)
+            VALUES
+            ($1, $2, $3, $4, NOW(), NOW())
+            RETURNING *
+        ",
+            params.first_name,
+            params.last_name,
+            params.email,
+            params.password_hash
+        )
+        .fetch_one(&self.db)
+        .await?)
+    }
+
+    async fn update(&self, id: i64, params: UpdateAccount) -> Result<AccountRow, Box<dyn Error>> {
+        Ok(sqlx::query_as!(
+            AccountRow,
+            "UPDATE account
+            SET first_name = $2, last_name = $3
+            WHERE id = $1
+            RETURNING *",
+            id,
+            params.first_name,
+            params.last_name
+        )
+        .fetch_one(&self.db)
+        .await?)
+    }
+
+    async fn delete(&self, id: i64) -> Result<AccountRow, Box<dyn Error>> {
+        Ok(sqlx::query_as!(
+            AccountRow,
+            "DELETE FROM account
+            WHERE id = $1
+            RETURNING *",
+            id
+        )
+        .fetch_one(&self.db)
+        .await?)
+    }
+
+    async fn get(&self, id: i64) -> Result<AccountRow, Box<dyn Error>> {
+        Ok(
+            sqlx::query_as!(AccountRow, "SELECT * FROM account WHERE id = $1", id)
+                .fetch_one(&self.db)
+                .await?,
+        )
+    }
+
+    async fn list(&self) -> Result<Vec<AccountRow>, Box<dyn Error>> {
+        Ok(sqlx::query_as!(AccountRow, "SELECT * FROM account")
+            .fetch_all(&self.db)
+            .await?)
+    }
+}
