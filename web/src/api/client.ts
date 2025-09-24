9a45e116ea8f1627bcb0863e4e1c5223bc577813
Konrad Schultz <konrad@seam.io>
2025-09-24 12:37:11 -0700

    Add api client file
    This will get split at some point.

---
+++ web/src/api/client.ts
@@ -0,0 +1,25 @@
+import type { AccountRes, CreateAccountReq } from "./generated/account";
+
+const parseRes = <T>(res: Response): Promise<T> => {
+  if (res.status >= 400) {
+    throw new Error(`${res.status}: ${res.url} request failed`);
+  }
+
+  return res.json();
+};
+
+type Account = Omit<AccountRes, "created_at" | "updated_at"> & {
+  created_at: Date;
+  updated_at: Date;
+};
+
+const createAccount = (req: CreateAccountReq): Promise<Account> =>
+  fetch("/account", { method: "POST", body: JSON.stringify(req) })
+    .then(parseRes<AccountRes>)
+    .then((res) => ({
+      ...res,
+      created_at: new Date(res.created_at),
+      updated_at: new Date(res.updated_at),
+    }));
+
+export type { Account };
